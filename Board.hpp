81e215a2cbdcb07d1a5e0e96ae096321045547db
CaveSpider1212 <aabhwan.adhikary@gmail.com>
2025-04-16 11:25:50 -0700

    Added #define constant statements

--- Board.hpp
+++ Board.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#define START_X 20 // starting x-coordinate of the board
+#define START_Y 20 // starting y-coordinate of the board
+#define BOARD_SIZE 10 // size of each side of the board (square)
+
 #include "Tile.hpp"
 
 class Board
@@ -10,5 +14,5 @@ public:
 
 	void generateBoard(sf::RenderWindow& window);
 private:
-	Tile* tiles[10][10]; // 9x9 board
+	Tile* tiles[BOARD_SIZE][BOARD_SIZE]; // 9x9 board
 };
